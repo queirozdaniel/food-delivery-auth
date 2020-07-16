99b71fbd51ad777283c0502a4a3ec94a8b08191d
queirozdaniel <danielqueirozak77@gmail.com>
2020-07-15 22:10:02 -0300

    Configurando RefreshToken

--- src/main/java/com/danielqueiroz/fooddelivery/auth/AuthorizationServerConfig.java
+++ src/main/java/com/danielqueiroz/fooddelivery/auth/AuthorizationServerConfig.java
@@ -3,11 +3,13 @@ package com.danielqueiroz.fooddelivery.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,19 +21,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager; 
 	
+	@Autowired
+	private UserDetailsService userDatailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 					.withClient("fooddelivery-web")
 					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password")
-					.scopes("write", "read");
+					.authorizedGrantTypes("password", "refresh_token")
+					.scopes("write", "read")
+					.accessTokenValiditySeconds(60 * 60 * 6)
+					.refreshTokenValiditySeconds(60 * 60 * 24 * 7);
 	}
 	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("isAuthenticated()");
+	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDatailsService);
 	}
 	
 	
